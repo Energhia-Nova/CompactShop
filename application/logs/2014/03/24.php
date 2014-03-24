084abeca5732a808a1317ca7c19129f89e90185e
Bawyka Taras <bawyka@gmail.com>
2014-03-24 11:52:50 +0200

    fixed projects controller

---
+++ application/logs/2014/03/24.php
@@ -0,0 +1,46 @@
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
+
+2014-03-24 11:43:09 --- CRITICAL: ErrorException [ 1 ]: Call to a member function headers() on a non-object ~ SYSPATH\classes\Kohana\Request\Client.php [ 119 ] in :
+2014-03-24 11:43:09 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
+2014-03-24 11:44:27 --- CRITICAL: ErrorException [ 1 ]: Call to a member function headers() on a non-object ~ SYSPATH\classes\Kohana\Request\Client.php [ 119 ] in :
+2014-03-24 11:44:27 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
+2014-03-24 11:45:07 --- CRITICAL: ErrorException [ 1 ]: Call to a member function headers() on a non-object ~ SYSPATH\classes\Kohana\Request\Client.php [ 119 ] in :
+2014-03-24 11:45:07 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
+2014-03-24 11:45:51 --- CRITICAL: ErrorException [ 1 ]: Call to a member function headers() on a non-object ~ SYSPATH\classes\Kohana\Request\Client.php [ 119 ] in :
+2014-03-24 11:45:51 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
+2014-03-24 11:46:33 --- CRITICAL: ErrorException [ 1 ]: Call to a member function headers() on a non-object ~ SYSPATH\classes\Kohana\Request\Client.php [ 119 ] in :
+2014-03-24 11:46:33 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
+2014-03-24 11:47:08 --- CRITICAL: ErrorException [ 1 ]: Using $this when not in object context ~ APPPATH\bootstrap.php [ 219 ] in :
+2014-03-24 11:47:08 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
+2014-03-24 11:47:36 --- CRITICAL: ErrorException [ 4096 ]: Argument 1 passed to Kohana_Route::name() must be an instance of Route, none given, called in C:\workspace\sites\mag\www\application\bootstrap.php on line 219 and defined ~ SYSPATH\classes\Kohana\Route.php [ 133 ] in C:\workspace\sites\mag\www\system\classes\Kohana\Route.php:133
+2014-03-24 11:47:36 --- DEBUG: #0 C:\workspace\sites\mag\www\system\classes\Kohana\Route.php(133): Kohana_Core::error_handler(4096, 'Argument 1 pass...', 'C:\workspace\si...', 133, Array)
+#1 C:\workspace\sites\mag\www\application\bootstrap.php(219): Kohana_Route::name()
+#2 C:\workspace\sites\mag\www\index.php(102): require('C:\workspace\si...')
+#3 {main} in C:\workspace\sites\mag\www\system\classes\Kohana\Route.php:133
+2014-03-24 11:48:11 --- CRITICAL: ErrorException [ 1 ]: Call to a member function headers() on a non-object ~ SYSPATH\classes\Kohana\Request\Client.php [ 119 ] in :
+2014-03-24 11:48:11 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
+2014-03-24 11:49:41 --- CRITICAL: ErrorException [ 1 ]: Call to a member function headers() on a non-object ~ SYSPATH\classes\Kohana\Request\Client.php [ 119 ] in :
+2014-03-24 11:49:41 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
+2014-03-24 11:49:46 --- CRITICAL: ErrorException [ 1 ]: Call to a member function send_headers() on a non-object ~ DOCROOT\index.php [ 119 ] in :
+2014-03-24 11:49:46 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
+2014-03-24 11:50:17 --- CRITICAL: ErrorException [ 1 ]: Call to a member function headers() on a non-object ~ SYSPATH\classes\Kohana\Request\Client.php [ 119 ] in :
+2014-03-24 11:50:17 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
+2014-03-24 11:50:36 --- CRITICAL: ErrorException [ 1 ]: Call to a member function send_headers() on a non-object ~ DOCROOT\index.php [ 119 ] in :
+2014-03-24 11:50:36 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
+2014-03-24 11:50:57 --- CRITICAL: ErrorException [ 1 ]: Call to a member function send_headers() on a non-object ~ DOCROOT\index.php [ 119 ] in :
+2014-03-24 11:50:57 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
+2014-03-24 11:51:17 --- CRITICAL: ErrorException [ 1 ]: Call to a member function send_headers() on a non-object ~ DOCROOT\index.php [ 119 ] in :
+2014-03-24 11:51:17 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
